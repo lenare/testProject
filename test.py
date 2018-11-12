36339e6c4c563dae80ed4d5e171df257579223fb
Lena Reichel <lena.reichel@witt-gruppe.eu>
2018-11-12 16:11:47 +0100

    updated test

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
+
+import os 
+from slackclient import SlackClient
+
+
+
+token = "xoxb-456010550053-478476771575-QwWRMkKl99HQcdIBKhQFJvdD"
+
+slack_client = SlackClient(token)
+
+
+
+
 print("test")
