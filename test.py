697fcb1a99ce827988996276bb541a7e076240b7
Lena Reichel <lena.reichel@witt-gruppe.eu>
2018-11-12 16:54:09 +0100

    updated test

--- test.py
+++ test.py
@@ -6,4 +6,13 @@ token = "xoxb-456010550053-478476771575-QwWRMkKl99HQcdIBKhQFJvdD"
 
 slack_client = SlackClient(token)
 
-print("test")
+bool = slack_client.rtm_connect(with_team_state=True)
+
+# slack_client.api_call("chat.postMessage", channel="C0")
+
+#Connect to slack
+if bool:
+    events = slack_client.rtm_read()
+
+print(bool)
+
